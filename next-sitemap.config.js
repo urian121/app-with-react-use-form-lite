ad93abce6b9dd16771aa1bd7ac5378ede9ead75a
URIAN VIERA <urian1213viera@gmail.com>
2025-05-02 15:21:54 -0500

    agregando sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: 'https://react-use-form-lite.vercel.app',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'Googlebot', allow: '/' },
+    ],
+  },
+};
