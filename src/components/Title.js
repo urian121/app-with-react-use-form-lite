cd275c0916bd388ea649ba1f4c23dbc3eff3019d
URIAN VIERA <urian1213viera@gmail.com>
2025-05-03 16:36:03 -0500

    agregando efecto al titulo

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 const Title = () => {
     return (
         <div className="text-center mt-5">
-            <h1 className="fw-bold display-4 text-ligth">react-use-form-lite</h1>
+            <h1 className="fw-bold display-4 text-light title-effect">react-use-form-lite</h1>
             <p className="fs-5 mx-auto" style={{ maxWidth: "800px", lineHeight: "1.4", color: "#fff", opacity: "0.7" }}>
                 Un Custom Hook moderno, intuitivo, liviano y flexible para manejar formularios en React sin dependencias adicionales.
                 Permite una implementación rápida y sin complicaciones, con soporte para inputs, selects, radios, checkboxes, archivos ¡y mucho más!
